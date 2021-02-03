1fa2b08b25d0bcadc4761a04f46062eeb069a709
slkhater <59323284+slk-hater@users.noreply.github.com>
2021-02-03 00:47:17 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,22 +8,22 @@ namespace Calculator
         {
             bool stop = false;
             string input;
-            ulong firstNumber, secondNumber;
+            ulong firstNumber, secondNumber, thirdNumber;
             byte operation;
             ConsoleKey keyPressed;
-            Console.Title = "Integer calculator - Guilherme Fadário";
+            Console.Title = "Long calculator - Guilherme Fadário";
             do
             {
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine("1 - add");
-                    Console.WriteLine("2 - subtract");
-                    Console.WriteLine("3 - multiply");
-                    Console.WriteLine("4 - divide");
-                    Console.WriteLine("5 - rest of division");
-                    Console.Write("Press a key to choose an operation: ");
-                    keyPressed = Console.ReadKey().Key;
+                    Console.WriteLine("Addition  +          (1)");
+                    Console.WriteLine("Subtract  -          (2)");
+                    Console.WriteLine("Multiply  *          (3)");
+                    Console.WriteLine("Divide  /            (4)");
+                    Console.WriteLine("Rest of division  %  (5)");
+                    Console.Write("Press a key[1,5] to choose an operation: ");
+                    keyPressed = Console.ReadKey(true).Key;
                 } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
                 do
                 {
@@ -38,12 +38,25 @@ namespace Calculator
                     Console.Write("Choose the second number: ");
                     input = Console.ReadLine();
                 } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
-                Console.Clear();
                 Console.WriteLine($"The output of ({firstNumber} {getOperation(operation)} {secondNumber}) is: {Calculate(getOperation(operation), firstNumber, secondNumber)}");
-                Console.WriteLine("Press ENTER to restart or ESCAPE to close");
-                keyPressed = Console.ReadKey().Key;
+                Console.WriteLine("Press TAB to continue, ENTER to restart or ESCAPE to close");
+                keyPressed = Console.ReadKey(true).Key;
                 if (keyPressed == ConsoleKey.Escape) stop = true; // close application
-            } while (!stop); 
+                else if(keyPressed == ConsoleKey.Tab && getOperation(operation) != "%")
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{Calculate(getOperation(operation), firstNumber, secondNumber)} {getOperation(operation)} ?");
+                        Console.Write("Choose the third number: ");
+                        input = Console.ReadLine();
+                    } while (!ulong.TryParse(input, out thirdNumber)); // input can't have letters, be null or empty and must fit into a ulong
+                    Console.WriteLine($"The output of ({Calculate(getOperation(operation), firstNumber, secondNumber)} {getOperation(operation)} {thirdNumber}) is: {Calculate(getOperation(operation), firstNumber, secondNumber, thirdNumber)}");
+                    Console.WriteLine("Press ENTER to restart or ESCAPE to close");
+                    keyPressed = Console.ReadKey(true).Key;
+                    if (keyPressed == ConsoleKey.Escape) stop = true; // close application
+                }
+            } while (!stop);
         }
         static bool inRange(int min, int max, int i)
         {
@@ -75,6 +88,19 @@ namespace Calculator
                     if (secondNumber != 0) return Convert.ToString(firstNumber % secondNumber);
                     else return "impossible"; // impossible operation
             }return null; // null path
+        }
+        static String Calculate(String operation, ulong firstNumber, ulong secondNumber, ulong thirdNumber)
+        {
+            switch (operation)
+            {
+                case "+": return Convert.ToString((firstNumber + secondNumber) + thirdNumber);
+                case "-": return Convert.ToString((firstNumber - secondNumber) - thirdNumber);
+                case "*": return Convert.ToString((firstNumber * secondNumber) * thirdNumber);
+                case "/":
+                    if (secondNumber != 0) return Convert.ToString((firstNumber / secondNumber) / thirdNumber);
+                    else return "impossible"; // impossible operation
+            }
+            return null; // null path
         }
     }
 }//by Gui1herme#8721
