f1bd9725376cdc5d0a1eed8c420336a9c64fd432
slkhater <59323284+slk-hater@users.noreply.github.com>
2021-02-03 20:05:00 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,54 +7,73 @@ namespace Calculator
         static void Main()
         {
             bool stop = false;
-            string input;
-            ulong firstNumber, secondNumber, thirdNumber;
+            string input, result;
+            ulong firstNumber, secondNumber;
             byte operation;
             ConsoleKey keyPressed;
             Console.Title = "Long calculator - Guilherme Fadário";
             do
             {
-                do
-                {
-                    Console.Clear();
-                    Console.WriteLine("Addition  +          (1)");
-                    Console.WriteLine("Subtract  -          (2)");
-                    Console.WriteLine("Multiply  *          (3)");
-                    Console.WriteLine("Divide  /            (4)");
-                    Console.WriteLine("Rest of division  %  (5)");
-                    Console.Write("Press a key[1,5] to choose an operation: ");
-                    keyPressed = Console.ReadKey(true).Key;
-                } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
-                do
-                {
-                    Console.Clear();
-                    Console.Write("Choose the first number: ");
-                    input = Console.ReadLine();
-                } while (!ulong.TryParse(input, out firstNumber)); // input can't have letters, be null or empty and must fit into a ulong
-                do
-                {
-                    Console.Clear();
-                    Console.WriteLine($"{firstNumber} {getOperation(operation)} ?");
-                    Console.Write("Choose the second number: ");
-                    input = Console.ReadLine();
-                } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
-                Console.WriteLine($"The output of ({firstNumber} {getOperation(operation)} {secondNumber}) is: {Calculate(getOperation(operation), firstNumber, secondNumber)}");
-                Console.WriteLine("Press TAB to continue, ENTER to restart or ESCAPE to close");
-                keyPressed = Console.ReadKey(true).Key;
-                if (keyPressed == ConsoleKey.Escape) stop = true; // close application
-                else if(keyPressed == ConsoleKey.Tab && getOperation(operation) != "%")
-                {
+                start:
                     do
                     {
                         Console.Clear();
-                        Console.WriteLine($"{Calculate(getOperation(operation), firstNumber, secondNumber)} {getOperation(operation)} ?");
-                        Console.Write("Choose the third number: ");
+                        Console.WriteLine("Addition          +  (1)");
+                        Console.WriteLine("Subtract          -  (2)");
+                        Console.WriteLine("Multiply          *  (3)");
+                        Console.WriteLine("Divide            /  (4)");
+                        Console.WriteLine("Rest of division  %  (5)");
+                        Console.Write("Press a key[1,5] to choose an operation: ");
+                        keyPressed = Console.ReadKey(true).Key;
+                    } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+                    do
+                    {
+                        Console.Clear();
+                        Console.Write("Choose the first number: ");
                         input = Console.ReadLine();
-                    } while (!ulong.TryParse(input, out thirdNumber)); // input can't have letters, be null or empty and must fit into a ulong
-                    Console.WriteLine($"The output of ({Calculate(getOperation(operation), firstNumber, secondNumber)} {getOperation(operation)} {thirdNumber}) is: {Calculate(getOperation(operation), firstNumber, secondNumber, thirdNumber)}");
-                    Console.WriteLine("Press ENTER to restart or ESCAPE to close");
+                    } while (!ulong.TryParse(input, out firstNumber)); // input can't have letters, be null or empty and must fit into a ulong
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{firstNumber} {getOperation(operation)} ?");
+                        Console.Write("Choose the second number: ");
+                        input = Console.ReadLine();
+                    } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
+                    result = Calculate(getOperation(operation), firstNumber, secondNumber);
+                    Console.WriteLine($"The output of ({firstNumber} {getOperation(operation)} {secondNumber}) is: {result}");
+                    Console.WriteLine("Press TAB to continue, ENTER to restart or ESCAPE to close");
                     keyPressed = Console.ReadKey(true).Key;
                     if (keyPressed == ConsoleKey.Escape) stop = true; // close application
+                if (!stop && keyPressed == ConsoleKey.Tab)
+                {
+                    do
+                    {
+                        do
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Previous calculation: {firstNumber} {getOperation(operation)} {secondNumber} = {result}");
+                            Console.WriteLine("Addition          +  (1)");
+                            Console.WriteLine("Subtract          -  (2)");
+                            Console.WriteLine("Multiply          *  (3)");
+                            Console.WriteLine("Divide            /  (4)");
+                            Console.WriteLine("Rest of division  %  (5)");
+                            Console.Write("Press a key[1,5] to choose another operation: ");
+                            keyPressed = Console.ReadKey(true).Key;
+                        } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+                        do
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"{result} {getOperation(operation)} ?");
+                            Console.Write("Choose the second number: ");
+                            input = Console.ReadLine();
+                        } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
+                        result = Calculate(getOperation(operation), Convert.ToUInt64(result), secondNumber);
+                        Console.WriteLine($"The output of ({firstNumber} {getOperation(operation)} {secondNumber}) is: {result}");
+                        Console.WriteLine("Press TAB to continue, ENTER to restart or ESCAPE to close");
+                        keyPressed = Console.ReadKey(true).Key;
+                        if (keyPressed == ConsoleKey.Escape) stop = true; // close application
+                        else if (keyPressed == ConsoleKey.Enter) goto start;
+                    } while (keyPressed == ConsoleKey.Tab);
                 }
             } while (!stop);
         }
@@ -88,19 +107,6 @@ namespace Calculator
                     if (secondNumber != 0) return Convert.ToString(firstNumber % secondNumber);
                     else return "impossible"; // impossible operation
             }return null; // null path
-        }
-        static String Calculate(String operation, ulong firstNumber, ulong secondNumber, ulong thirdNumber)
-        {
-            switch (operation)
-            {
-                case "+": return Convert.ToString((firstNumber + secondNumber) + thirdNumber);
-                case "-": return Convert.ToString((firstNumber - secondNumber) - thirdNumber);
-                case "*": return Convert.ToString((firstNumber * secondNumber) * thirdNumber);
-                case "/":
-                    if (secondNumber != 0) return Convert.ToString((firstNumber / secondNumber) / thirdNumber);
-                    else return "impossible"; // impossible operation
-            }
-            return null; // null path
         }
     }
 }//by Gui1herme#8721
