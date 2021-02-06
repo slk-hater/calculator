8763d9ade3dcaa91ce77bb9c6891b89c455449e0
slkhater <59323284+slk-hater@users.noreply.github.com>
2021-02-06 22:54:33 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,7 +7,7 @@ namespace Calculator
         static void Main()
         {
         string input, result;
-        byte operation;
+        byte decision;
         ulong firstNumber, secondNumber;
         ConsoleKey keyPressed;
         Console.Title = "Long calculator - Guilherme Fadário";
@@ -21,11 +21,13 @@ namespace Calculator
                 Console.WriteLine("Divide                /  (4)");
                 Console.WriteLine("Rest of division      %  (5)");
                 Console.WriteLine("");
-                Console.WriteLine("Exit the console         (6)");
-                Console.Write("Press a key[1,6] to choose an operation: ");
+                Console.WriteLine("Change console color     (6)");
+                Console.WriteLine("Exit the console         (7)");
+                Console.Write("Press a key[1,7] to choose an operation: ");
                 keyPressed = Console.ReadKey(true).Key;
-                if (keyPressed.ToString().Replace("D", null) == "6") goto ExitConsole;
-            } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+                if (keyPressed.ToString().Replace("D", null) == "6") goto ChangeColor;
+                if (keyPressed.ToString().Replace("D", null) == "7") goto ExitConsole;
+            } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out decision) || !inRange(1, 5, decision)); // keyPressed can only be between 1 and 5 ; D1 -> 1
             do
             {
                 Console.Clear();
@@ -35,11 +37,11 @@ namespace Calculator
             do
             {
                 Console.Clear();
-                Console.WriteLine($"{firstNumber} {getOperation(operation)} ?");
+                Console.WriteLine($"{firstNumber} {getOperation(decision)} ?");
                 Console.Write("Choose the second number: ");
                 input = Console.ReadLine();
             } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
-            result = Calculate(getOperation(operation), firstNumber, secondNumber);
+            result = Calculate(getOperation(decision), firstNumber, secondNumber);
             goto SecondPhase;
         SecondPhase:
             do
@@ -47,7 +49,7 @@ namespace Calculator
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine($"Previous calculation: {firstNumber} {getOperation(operation)} {secondNumber} = {result}");
+                    Console.WriteLine($"Previous calculation: {firstNumber} {getOperation(decision)} {secondNumber} = {result}");
                     Console.WriteLine("Add                   +  (1)");
                     Console.WriteLine("Subtract              -  (2)");
                     Console.WriteLine("Multiply              *  (3)");
@@ -62,17 +64,46 @@ namespace Calculator
                     if (keyPressed.ToString().Replace("D", null) == "6") result = "0";
                     if (keyPressed.ToString().Replace("D", null) == "7") goto Start;
                     if (keyPressed.ToString().Replace("D", null) == "8") goto ExitConsole;
-                } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out operation) || !inRange(1, 5, operation)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+                } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out decision) || !inRange(1, 5, decision)); // keyPressed can only be between 1 and 5 ; D1 -> 1
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine($"{result} {getOperation(operation)} ?");
+                    Console.WriteLine($"{result} {getOperation(decision)} ?");
                     Console.Write("Choose the second number: ");
                     input = Console.ReadLine();
                 } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
                 firstNumber = Convert.ToUInt64(result);
-                result = Calculate(getOperation(operation), Convert.ToUInt64(result), secondNumber);
+                result = Calculate(getOperation(decision), Convert.ToUInt64(result), secondNumber);
             } while (true);
+        ChangeColor:
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("White               (1)");
+                Console.WriteLine("Black               (2)");
+                Console.WriteLine("Red                 (3)");
+                Console.WriteLine("Blue                (4)");
+                Console.WriteLine("Green               (5)");
+                Console.WriteLine("Cyan                (6)");
+                Console.Write("Press a key[1,6] to choose a color: ");
+                keyPressed = Console.ReadKey(true).Key;
+            } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out decision) || !inRange(1, 6, decision)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+            switch (decision)
+            {
+                case 1: Console.BackgroundColor = ConsoleColor.White;
+                    break;
+                case 2: Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                case 3: Console.BackgroundColor = ConsoleColor.DarkRed;
+                    break;
+                case 4: Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case 5: Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case 6: Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    break;
+            }
+            goto Start;
         ExitConsole:
             Console.Beep();
         }
