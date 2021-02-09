368fc8ef908f6808cc4105480e0a73b22a0a79d2
slkhater <59323284+slk-hater@users.noreply.github.com>
2021-02-09 00:13:12 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,41 +7,43 @@ namespace Calculator
         static void Main()
         {
         string input, result;
-        byte decision;
-        ulong firstNumber, secondNumber;
+        byte decision, precisionPreference=2;
+        double firstOperand, secondOperand;
         ConsoleKey keyPressed;
-        Console.Title = "Long calculator - Guilherme Fadário";
+        Console.Title = "Numbers calculator - Guilherme Fadário";
         Start:
             do
             {
                 Console.Clear();
-                Console.WriteLine("Add                   +  (1)");
-                Console.WriteLine("Subtract              -  (2)");
-                Console.WriteLine("Multiply              *  (3)");
-                Console.WriteLine("Divide                /  (4)");
-                Console.WriteLine("Rest of division      %  (5)");
+                Console.WriteLine("Add                   +   (1)");
+                Console.WriteLine("Subtract              -   (2)");
+                Console.WriteLine("Multiply              *   (3)");
+                Console.WriteLine("Divide                /   (4)");
+                Console.WriteLine("Rest of division      %   (5)");
                 Console.WriteLine("");
-                Console.WriteLine("Change console color     (C)");
-                Console.WriteLine("Exit the console         (E)");
+                Console.WriteLine("Change console color      (C)");
+                Console.WriteLine("Decimal places preference (D)");
+                Console.WriteLine("Exit the console          (E)");
                 Console.Write("Press a key[1,5] to choose an operation: ");
                 keyPressed = Console.ReadKey(true).Key;
-                if (keyPressed.ToString().Replace("D", null) == "C") goto ChangeColor;
-                if (keyPressed.ToString().Replace("D", null) == "E") goto ExitConsole;
+                if (keyPressed == ConsoleKey.C) goto ChangeColor;
+                else if (keyPressed == ConsoleKey.D) goto ChangePrecisionPreference;
+                else if (keyPressed == ConsoleKey.E) goto ExitConsole;
             } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out decision) || !inRange(1, 5, decision)); // keyPressed can only be between 1 and 5 ; D1 -> 1
             do
             {
                 Console.Clear();
-                Console.Write("Choose the first number: ");
+                Console.Write("Choose the first operand: ");
                 input = Console.ReadLine();
-            } while (!ulong.TryParse(input, out firstNumber)); // input can't have letters, be null or empty and must fit into a ulong
+            } while (!double.TryParse(input, out firstOperand)); // input can't have letters, be null or empty and must fit into a double
             do
             {
                 Console.Clear();
-                Console.WriteLine($"{firstNumber} {getOperation(decision)} ?");
-                Console.Write("Choose the second number: ");
+                Console.WriteLine($"{Math.Round(firstOperand, precisionPreference)} {getOperation(decision)} ?");
+                Console.Write("Choose the second operand: ");
                 input = Console.ReadLine();
-            } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
-            result = Calculate(getOperation(decision), firstNumber, secondNumber);
+            } while (!double.TryParse(input, out secondOperand)); // input can't have letters, be null or empty and must fit into a double
+            result = Calculate(getOperation(decision), firstOperand, secondOperand);
             goto SecondPhase;
         SecondPhase:
             do
@@ -50,16 +52,16 @@ namespace Calculator
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine($"Previous calculation: {firstNumber} {lastOperation} {secondNumber} = {result}");
-                    Console.WriteLine("Add                   +  (1)");
-                    Console.WriteLine("Subtract              -  (2)");
-                    Console.WriteLine("Multiply              *  (3)");
-                    Console.WriteLine("Divide                /  (4)");
-                    Console.WriteLine("Rest of division      %  (5)");
+                    Console.WriteLine($"Previous calculation: {Math.Round(firstOperand, precisionPreference)} {lastOperation} {Math.Round(secondOperand, precisionPreference)} = {Math.Round(Convert.ToDouble(result), precisionPreference)}");
+                    Console.WriteLine("Add                   +   (1)");
+                    Console.WriteLine("Subtract              -   (2)");
+                    Console.WriteLine("Multiply              *   (3)");
+                    Console.WriteLine("Divide                /   (4)");
+                    Console.WriteLine("Rest of division      %   (5)");
                     Console.WriteLine("");
-                    Console.WriteLine("Clear the result         (C)");
-                    Console.WriteLine("Restart the console      (R)");
-                    Console.WriteLine("Exit the console         (E)");
+                    Console.WriteLine("Clear the result          (C)");
+                    Console.WriteLine("Restart the console       (R)");
+                    Console.WriteLine("Exit the console          (E)");
                     Console.Write("Press a key[1,5] to choose an operation: ");
                     keyPressed = Console.ReadKey(true).Key;
                     if (keyPressed == ConsoleKey.C) result = "0";
@@ -69,32 +71,32 @@ namespace Calculator
                 do
                 {
                     Console.Clear();
-                    Console.WriteLine($"{result} {getOperation(decision)} ?");
-                    Console.Write("Choose the second number: ");
+                    Console.WriteLine($"{Math.Round(Convert.ToDouble(result), precisionPreference)} {getOperation(decision)} ?");
+                    Console.Write("Choose the second operand: ");
                     input = Console.ReadLine();
-                } while (!ulong.TryParse(input, out secondNumber)); // input can't have letters, be null or empty and must fit into a ulong
-                firstNumber = Convert.ToUInt64(result);
-                result = Calculate(getOperation(decision), Convert.ToUInt64(result), secondNumber);
+                } while (!double.TryParse(input, out secondOperand)); // input can't have letters, be null or empty and must fit into a double
+                firstOperand = Convert.ToDouble(result);
+                result = Calculate(getOperation(decision), Convert.ToDouble(result), secondOperand);
             } while (true);
         ChangeColor:
             do
             {
                 Console.Clear();
                 Console.WriteLine("Background Color:");
-                Console.WriteLine(" Black                   (1)");
-                Console.WriteLine(" Red                     (2)");
-                Console.WriteLine(" Blue                    (3)");
-                Console.WriteLine(" Green                   (4)");
-                Console.WriteLine(" Cyan                    (5)");
+                Console.WriteLine(" Black                    (1)");
+                Console.WriteLine(" Red                      (2)");
+                Console.WriteLine(" Blue                     (3)");
+                Console.WriteLine(" Green                    (4)");
+                Console.WriteLine(" Cyan                     (5)");
                 Console.WriteLine("Foreground Color:");
-                Console.WriteLine(" Black                   (6)");
-                Console.WriteLine(" Red                     (7)");
-                Console.WriteLine(" Blue                    (8)");
-                Console.WriteLine(" Green                   (9)");
-                Console.WriteLine(" Cyan                    (0)");
+                Console.WriteLine(" Black                    (6)");
+                Console.WriteLine(" Red                      (7)");
+                Console.WriteLine(" Blue                     (8)");
+                Console.WriteLine(" Green                    (9)");
+                Console.WriteLine(" Cyan                     (0)");
                 Console.WriteLine("");
-                Console.WriteLine("Reset both colors        (R)");
-                Console.WriteLine("Exit this menu           (E)");
+                Console.WriteLine("Reset both colors         (R)");
+                Console.WriteLine("Exit this menu            (E)");
                 Console.Write("Press a key[0,9] to choose a color: ");
                 keyPressed = Console.ReadKey(true).Key;
                 if(keyPressed == ConsoleKey.R)
@@ -128,6 +130,26 @@ namespace Calculator
                     break;
             }
             goto ChangeColor;
+        ChangePrecisionPreference:
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Current decimal places preference: {precisionPreference}");
+                Console.WriteLine("1 DPP                     (1)");
+                Console.WriteLine("2 DPP (default)           (2)");
+                Console.WriteLine("3 DPP                     (3)");
+                Console.WriteLine("4 DPP                     (4)");
+                Console.WriteLine("5 DPP                     (5)");
+                Console.WriteLine("");
+                Console.WriteLine("Reset preference          (R)");
+                Console.WriteLine("Exit this menu            (E)");
+                Console.Write("Press a key[1,5] to choose a color: ");
+                keyPressed = Console.ReadKey(true).Key;
+                if (keyPressed == ConsoleKey.R) precisionPreference = 2;
+                else if (keyPressed == ConsoleKey.E) goto Start;
+            } while (!byte.TryParse(keyPressed.ToString().Replace("D", null), out decision) || !inRange(1, 5, decision)); // keyPressed can only be between 1 and 5 ; D1 -> 1
+            precisionPreference = decision;
+            goto ChangePrecisionPreference;
         ExitConsole:
             Console.Beep();
         }
@@ -143,14 +165,14 @@ namespace Calculator
                 case 5: return "%";
             }return null; // null path
         }
-        static String Calculate(String operation, ulong num1, ulong num2)
+        static String Calculate(String operation, double num1, double num2)
         {
             switch (operation)
             {
                 case "+": return Convert.ToString(num1 + num2);
                 case "-":
                     if (num1 > num2) return Convert.ToString(num1 - num2);
-                    else return "0"; // impossible operation for ulong
+                    else return "0"; // impossible operation for double
                 case "*": return Convert.ToString(num1 * num2);
                 case "/":
                     if (num2 != 0) return Convert.ToString(num1 / num2);
