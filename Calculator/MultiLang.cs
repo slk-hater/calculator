b2507fbb4dbf93f7c165bea5540cacb2b9836c36
slkhater <59323284+slk-hater@users.noreply.github.com>
2021-02-21 03:04:33 +0000

    Update MultiLang.cs

--- Calculator/MultiLang.cs
+++ Calculator/MultiLang.cs
@@ -1,27 +1,31 @@
-using System;
-
 namespace Calculator
 {
     public class MultiLang
     {
-        public static string messageByLang(String lang, MessagesEnum msg)
+        public static string StringByLang(string en, string pt)
         {
-            if (lang == "en-US") //case Language is English
+            if (Program.language == "en-US") return en;
+            else if (Program.language == "pt-PT") return pt;
+            return null;
+        }
+        public static string MessageByLang(MessagesEnum msg)
+        {
+            if (Program.language == "en-US") //case Language is English
             {
                 switch (msg)
                 {
-                    case MessagesEnum.CONSOLE_TITLE: return "Numbers calculator - Guilherme Fadário";
+                    case MessagesEnum.CONSOLE_TITLE: return "Standard calculator - Guilherme Fadário";
                     case MessagesEnum.MAIN_MENU: return "Calculator - Main menu";
                     case MessagesEnum.ADD: return " Add                   +    (1)";
                     case MessagesEnum.SUBTRACT: return " Subtract              -    (2)";
-                    case MessagesEnum.MULTIPLY: return " Multiply              *    (3)";
-                    case MessagesEnum.DIVIDE: return " Divide                /    (4)";
-                    case MessagesEnum.REST_OF_DIVISION: return " Rest of division      %    (5)";
+                    case MessagesEnum.MULTIPLY: return " Multiply              x    (3)";
+                    case MessagesEnum.DIVIDE: return " Divide                ÷    (4)";
+                    case MessagesEnum.REST_OF_DIVISION: return " Remainder             %    (5)";
                     case MessagesEnum.SETTINGS: return "Settings                    (S)";
                     case MessagesEnum.SETTINGS_MENU: return "Calculator - Settings menu";
-                    case MessagesEnum.CHANGE_LANGUAGE: return " Change language            (L)";
+                    case MessagesEnum.CHANGE_LANGUAGE: return " Language                   (L)";
                     case MessagesEnum.LANGUAGE_MENU: return "Calculator - Languages menu";
-                    case MessagesEnum.CHANGE_COLOR: return " Change console color       (C)";
+                    case MessagesEnum.CHANGE_COLOR: return " Console color              (C)";
                     case MessagesEnum.COLORS_MENU: return "Calculator - Colors menu";
                     case MessagesEnum.DECIMAL_PLACES_PREFERENCE: return " Decimal places preference  (D)";
                     case MessagesEnum.DECIMAL_PLACES_PREFERENCE_MENU: return "Calculator - Decimal places menu";
@@ -32,22 +36,22 @@ namespace Calculator
                     case MessagesEnum.ASK_FOR_KEY: return "Press a key[@keys] to choose a @type: ";
                 }
             }
-            else if (lang == "pt-PT") //case Language is Portuguese
+            else if (Program.language == "pt-PT") //case Language is Portuguese
             {
                 switch (msg)
                 {
-                    case MessagesEnum.CONSOLE_TITLE: return "Calculadora de números - Guilherme Fadário";
+                    case MessagesEnum.CONSOLE_TITLE: return "Calculadora padrão - Guilherme Fadário";
                     case MessagesEnum.MAIN_MENU: return "Calculadora - Menu principal";
                     case MessagesEnum.ADD: return " Adicionar             +    (1)";
                     case MessagesEnum.SUBTRACT: return " Subtrair              -    (2)";
-                    case MessagesEnum.MULTIPLY: return " Multiplicar           *    (3)";
-                    case MessagesEnum.DIVIDE: return " Dividir               /    (4)";
+                    case MessagesEnum.MULTIPLY: return " Multiplicar           x    (3)";
+                    case MessagesEnum.DIVIDE: return " Dividir               ÷    (4)";
                     case MessagesEnum.REST_OF_DIVISION: return " Resto de divisão      %    (5)";
                     case MessagesEnum.SETTINGS: return "Definições                  (S)";
                     case MessagesEnum.SETTINGS_MENU: return "Calculadora - Menu de definições";
-                    case MessagesEnum.CHANGE_LANGUAGE: return " Alterar linguagem          (L)";
+                    case MessagesEnum.CHANGE_LANGUAGE: return " Linguagem                  (L)";
                     case MessagesEnum.LANGUAGE_MENU: return "Calculadora - Menu de linguagens";
-                    case MessagesEnum.CHANGE_COLOR: return " Alterar cor da consola     (C)";
+                    case MessagesEnum.CHANGE_COLOR: return " Cor da consola             (C)";
                     case MessagesEnum.COLORS_MENU: return "Calculadora - Menu de cores";
                     case MessagesEnum.DECIMAL_PLACES_PREFERENCE: return " Preferência casas decimais (D)";
                     case MessagesEnum.DECIMAL_PLACES_PREFERENCE_MENU: return "Calculadora - Menu de casas decimais";
@@ -82,12 +86,6 @@ namespace Calculator
             EXIT_CONSOLE,
             EXIT_THIS_MENU,
             ASK_FOR_KEY
-        }
-        public static string stringByLang(string lang, string t, string f)
-        {
-            if (lang == "en-US") return t;
-            else if (lang == "pt-PT") return f;
-            return null;
         }
     }
 }
